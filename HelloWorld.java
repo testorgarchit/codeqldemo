c4ba17f71ecd6876f5d926558185e46a4ce58ad7
shrikantarchit <shrikantarchit@gmail.com>
2023-02-11 19:47:42 +0530

    second commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,18 @@
+import java.sql.DriverManager;
+import java.sql.SQLException;
 class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!"); 
+		 System.out.println("Hello, World!");
+        String id = "admin"; // hard-coded credential
+        String password = "password"; // hard-coded credential
+       
+	   getConn(id,"password");
+		
     }
+	
+	public static void getConn(String id, String password) throws SQLException {
+   
+    DriverManager.getConnection(id, "password",password); // sensitive call
+}
 }
