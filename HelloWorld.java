703f5b6e964e09784a5bd3d2086af08d2cf2d804
shrikantarchit <109203106+shrikantarchit@users.noreply.github.com>
2023-02-11 19:53:23 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 import java.sql.DriverManager;
 import java.sql.SQLException;
 class HelloWorld {
-    public static void main(String[] args) {
+    public static void main(String[] args)  throws SQLException{
         System.out.println("Hello, World!"); 
 		 System.out.println("Hello, World!");
         String id = "admin"; // hard-coded credential
